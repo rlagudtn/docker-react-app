a448b5b37b2786a774209dbfac8cc0561ad43023
rlagudtn <rlagudtn4510@naver.com>
2022-07-31 19:29:01 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn sssssdddgf  d
         </a>
       </header>
     </div>
